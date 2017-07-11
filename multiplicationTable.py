18fa2d5cc8ce50d346c8e35f037b46853381ad91
dmli30 <dmlifei@gmail.com>
2017-07-11 14:44:39 +0800

    Update multiplicationTable.py

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -2,15 +2,15 @@
 import openpyxl
 from openpyxl.styles import Font
 wb = openpyxl.Workbook()
-sheet = wb.get_active_sheet()
+ws = wb.active
 N=int(input('please input your number: '))
 for i in range(2,N+2):    #初始化行1和列A
-    sheet.cell(row=i, column=1).value = i - 1
-    sheet.cell(row=i, column=1).font = Font(bold=True)
-    sheet.cell(row=1, column=i).value = i - 1
-    sheet.cell(row=1, column=i).font = Font(bold=True)
+    ws.cell(row=i, column=1).value = i - 1
+    ws.cell(row=i, column=1).font = Font(bold=True)
+    ws.cell(row=1, column=i).value = i - 1
+    ws.cell(row=1, column=i).font = Font(bold=True)
 for rowNum in range(2,N+2):   #计算填充乘法表
     for columnNum in range(2,N+2):
-        sheet.cell(row=rowNum,column=columnNum).value = sheet.cell(row=rowNum,column=1).value * sheet.cell(row=1,column=columnNum).value
+        ws.cell(row=rowNum,column=columnNum).value = ws.cell(row=rowNum,column=1).value * ws.cell(row=1,column=columnNum).value
 filename = str(N) + 'x' + str(N) + '.xlsx'
 wb.save(filename)
