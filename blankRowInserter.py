04fac24bf9d05de1a82b31c802a19bba14029cd3
dmli30 <dmlifei@gmail.com>
2017-07-12 11:50:01 +0800

    Update blankRowInserter.py

--- blankRowInserter.py
+++ blankRowInserter.py
@@ -2,24 +2,28 @@
 import openpyxl
 from openpyxl.utils import column_index_from_string
 
+def rowCopy (ws1,row1,ws2,row2):
+    for cell in ws1[row1]:
+        ws2.cell(row=row2,column=column_index_from_string(cell.column)).value = cell.value
+    return 0
+
 def blankRowInserter (N, M, fileName):
     wb1 = openpyxl.load_workbook(fileName)
+    wb2 = openpyxl.Workbook()
     ws1 = wb1.active
-    if N > ws1.max_row:   #如果N大于总行数，则不用插入，因为本来也是空的
-        print('This excel is too small to do anything,try another file!')
-        return 0
+    ws2 = wb2.active
+    if N == 1:
+        for rowNum in range(1,ws1.max_row + 1):
+            rowCopy(ws1,rowNum,ws2,rowNum + M)
+    elif N > 1 and N <= ws1.max_row:
+        for rowNum in range(1,N):
+            rowCopy(ws1,rowNum,ws2,rowNum)
+        for rowNum in range(N,ws1.max_row + 1):
+            rowCopy(ws1,rowNum,ws2,rowNum + M)
     else:
-        wb2 = openpyxl.Workbook()
-        ws2 = wb2.active
-        for rowOfCell in ws1[1:N-1]:    #两层循环，第一层得到一个由单元格元组组成的元组
-            for cell in rowOfCell:    #第二层得到单元格，两个for循环拷贝前N行
-                ws2[cell.coordinate].value = cell.value
-        for rowOfCell in ws1[N:ws1.max_row]:    #对于剩下的行，行号加M再进行拷贝
-            for cell in rowOfCell:
-               ws2.cell(row = cell.row + M, column = column_index_from_string(cell.column)).value = cell.value    #用到了column字母转数字
-        print('work done!')
-        wb2.save(fileName)
         return 0
+    wb1.close()
+    wb2.save(fileName)
 
 N = int(input('please input the row number: '))
 M = int(input('how many lines would you like to insert: '))
